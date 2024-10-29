b1ee8f50d578d55406164fd896e1a9f14a3e4a56
chachacode <mezovt123@gmail.com>
2024-10-29 18:52:14 +0500

    ewt23

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -5,17 +5,6 @@ export default function Footer() {
   return (
     <Box position="fixed" bottom="0" width="100%">
       <MessageForm />
-
-      <Grid
-        gridTemplateColumns="auto 1fr"
-        textAlign="center"
-        alignItems="center"
-        py="4px"
-        px="30px"
-        height="40px"
-        bg="white"
-      >
-      </Grid>
     </Box>
   );
 }
