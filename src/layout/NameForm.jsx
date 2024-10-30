6b9fd2fc782715b02e1edd26a7dc3be7e3395bf1
chachacode <mezovt123@gmail.com>
2024-10-30 15:54:42 +0500

    вап35235

--- src/layout/NameForm.jsx
+++ src/layout/NameForm.jsx
@@ -1,69 +1,18 @@
-import { useEffect, useRef, useState } from "react";
-import { Input, Stack, IconButton, Text } from "@chakra-ui/react";
-import { BiSave, BiEdit } from "react-icons/bi";
+import { Stack, Text } from "@chakra-ui/react";
 import { useAppContext } from "../context/appContext";
 
 export default function NameForm() {
-  const { username, setUsername } = useAppContext();
-  const [newUsername, setNewUsername] = useState(username);
-  const [isEditing, setIsEditing] = useState(false);
-  const toggleEditing = () => {
-    setIsEditing(!isEditing);
-  };
-
-  const inputRef = useRef(null);
-
-  useEffect(() => {
-    if (isEditing) {
-      inputRef.current.focus();
-    }
-  }, [isEditing]);
-
-  useEffect(() => {
-    setNewUsername(username);
-  }, [username]);
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    toggleEditing();
-
-    if (!newUsername) {
-      setNewUsername(username);
-      return;
-    }
-
-    setUsername(newUsername);
-    localStorage.setItem("username", newUsername);
-  };
+  const { username } = useAppContext();
 
   return (
-    <form onSubmit={handleSubmit} style={{ marginLeft: "20px" }}>
-      <Stack direction="row" alignItems="center" justifyContent="center" width="100%">
-        {isEditing ? (
-          <Input
-            name="username"
-            placeholder="Choose a username"
-            onChange={(e) => setNewUsername(e.target.value)}
-            value={newUsername}
-            bg="#1C1C1C"
-            size="sm"
-            border="none"
-            onBlur={handleSubmit}
-            ref={inputRef}
-            maxLength="15"
-          />
-        ) : (
-          <Text 
-            onClick={toggleEditing} 
-            cursor="pointer" 
-            width="100%" 
-            textAlign="center" 
-            flexGrow={1}
-          >
-            Чат и ты <strong>{newUsername}</strong>
-          </Text>
-        )}
-      </Stack>
-    </form>
+    <Stack direction="row" alignItems="center" justifyContent="center" width="100%" style={{ marginLeft: "20px" }}>
+      <Text 
+        width="100%" 
+        textAlign="center" 
+        flexGrow={1}
+      >
+        Чат и ты <strong>{username}</strong>
+      </Text>
+    </Stack>
   );
 }
